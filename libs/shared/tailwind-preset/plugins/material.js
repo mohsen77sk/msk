29cccabb50c2e5ac6b09132cff099ad70974e284
mohsen77sk <mohsen77sk@gmail.com>
2025-02-12 17:47:03 +0330

    update package and styles

--- libs/shared/tailwind-preset/plugins/material.js
+++ libs/shared/tailwind-preset/plugins/material.js
@@ -8,203 +8,203 @@ module.exports = plugin(({ addComponents, addUtilities, theme }) => {
    */
   addUtilities({
     '.bg-default': {
-      backgroundColor: 'var(--sys-surface-container) !important',
+      backgroundColor: 'var(--mat-sys-surface-container) !important',
     },
     '.text-default': {
-      color: 'var(--sys-on-surface) !important',
+      color: 'var(--mat-sys-on-surface) !important',
     },
 
     '.bg-surface': {
-      backgroundColor: 'var(--sys-surface) !important',
+      backgroundColor: 'var(--mat-sys-surface) !important',
     },
     '.bg-surface-bright': {
-      backgroundColor: 'var(--sys-surface-bright) !important',
+      backgroundColor: 'var(--mat-sys-surface-bright) !important',
     },
     '.bg-surface-container': {
-      backgroundColor: 'var(--sys-surface-container) !important',
+      backgroundColor: 'var(--mat-sys-surface-container) !important',
     },
     '.bg-surface-container-lowest': {
-      backgroundColor: 'var(--sys-surface-container-lowest) !important',
+      backgroundColor: 'var(--mat-sys-surface-container-lowest) !important',
     },
     '.bg-surface-container-low': {
-      backgroundColor: 'var(--sys-surface-container-low) !important',
+      backgroundColor: 'var(--mat-sys-surface-container-low) !important',
     },
     '.bg-surface-container-high': {
-      backgroundColor: 'var(--sys-surface-container-high) !important',
+      backgroundColor: 'var(--mat-sys-surface-container-high) !important',
     },
     '.bg-surface-container-highest': {
-      backgroundColor: 'var(--sys-surface-container-highest) !important',
+      backgroundColor: 'var(--mat-sys-surface-container-highest) !important',
     },
     '.bg-surface-dim': {
-      backgroundColor: 'var(--sys-surface-dim) !important',
+      backgroundColor: 'var(--mat-sys-surface-dim) !important',
     },
     '.bg-surface-tint': {
-      backgroundColor: 'var(--sys-surface-tint) !important',
+      backgroundColor: 'var(--mat-sys-surface-tint) !important',
     },
     '.bg-surface-variant': {
-      backgroundColor: 'var(--sys-surface-variant) !important',
+      backgroundColor: 'var(--mat-sys-surface-variant) !important',
     },
 
     '.text-on-surface': {
-      color: 'var(--sys-on-surface) !important',
+      color: 'var(--mat-sys-on-surface) !important',
     },
     '.text-on-surface-variant': {
-      color: 'var(--sys-on-surface-variant) !important',
+      color: 'var(--mat-sys-on-surface-variant) !important',
     },
 
     '.bg-inverse-surface': {
-      backgroundColor: 'var(--sys-inverse-surface) !important',
+      backgroundColor: 'var(--mat-sys-inverse-surface) !important',
     },
     '.text-inverse-on-surface': {
-      color: 'var(--sys-inverse-on-surface) !important',
+      color: 'var(--mat-sys-inverse-on-surface) !important',
     },
 
     '.bg-primary': {
-      backgroundColor: 'var(--sys-primary) !important',
+      backgroundColor: 'var(--mat-sys-primary) !important',
     },
     '.text-primary': {
-      color: 'var(--sys-primary) !important',
+      color: 'var(--mat-sys-primary) !important',
     },
     '.text-on-primary': {
-      color: 'var(--sys-on-primary) !important',
+      color: 'var(--mat-sys-on-primary) !important',
     },
     '.bg-primary-container': {
-      backgroundColor: 'var(--sys-primary-container) !important',
+      backgroundColor: 'var(--mat-sys-primary-container) !important',
     },
     '.text-on-primary-container': {
-      color: 'var(--sys-on-primary-container) !important',
+      color: 'var(--mat-sys-on-primary-container) !important',
     },
     '.bg-primary-fixed': {
-      backgroundColor: 'var(--sys-primary-fixed) !important',
+      backgroundColor: 'var(--mat-sys-primary-fixed) !important',
     },
     '.bg-primary-fixed-dim': {
-      backgroundColor: 'var(--sys-primary-fixed-dim) !important',
+      backgroundColor: 'var(--mat-sys-primary-fixed-dim) !important',
     },
     '.text-on-primary-fixed': {
-      color: 'var(--sys-on-primary-fixed) !important',
+      color: 'var(--mat-sys-on-primary-fixed) !important',
     },
     '.text-on-primary-fixed-variant': {
-      color: 'var(--sys-on-primary-fixed-variant) !important',
+      color: 'var(--mat-sys-on-primary-fixed-variant) !important',
     },
 
     '.bg-secondary': {
-      backgroundColor: 'var(--sys-secondary) !important',
+      backgroundColor: 'var(--mat-sys-secondary) !important',
     },
     '.text-secondary': {
-      color: 'var(--sys-secondary) !important',
+      color: 'var(--mat-sys-secondary) !important',
     },
     '.text-on-secondary': {
-      color: 'var(--sys-on-secondary) !important',
+      color: 'var(--mat-sys-on-secondary) !important',
     },
     '.bg-secondary-container': {
-      backgroundColor: 'var(--sys-secondary-container) !important',
+      backgroundColor: 'var(--mat-sys-secondary-container) !important',
     },
     '.text-on-secondary-container': {
-      color: 'var(--sys-on-secondary-container) !important',
+      color: 'var(--mat-sys-on-secondary-container) !important',
     },
     '.bg-secondary-fixed': {
-      backgroundColor: 'var(--sys-secondary-fixed) !important',
+      backgroundColor: 'var(--mat-sys-secondary-fixed) !important',
     },
     '.bg-secondary-fixed-dim': {
-      backgroundColor: 'var(--sys-secondary-fixed-dim) !important',
+      backgroundColor: 'var(--mat-sys-secondary-fixed-dim) !important',
     },
     '.text-on-secondary-fixed': {
-      color: 'var(--sys-on-secondary-fixed) !important',
+      color: 'var(--mat-sys-on-secondary-fixed) !important',
     },
     '.text-on-secondary-fixed-variant': {
-      color: 'var(--sys-on-secondary-fixed-variant) !important',
+      color: 'var(--mat-sys-on-secondary-fixed-variant) !important',
     },
 
     '.bg-tertiary': {
-      backgroundColor: 'var(--sys-tertiary) !important',
+      backgroundColor: 'var(--mat-sys-tertiary) !important',
     },
     '.text-tertiary': {
-      color: 'var(--sys-tertiary) !important',
+      color: 'var(--mat-sys-tertiary) !important',
     },
     '.text-on-tertiary': {
-      color: 'var(--sys-on-tertiary) !important',
+      color: 'var(--mat-sys-on-tertiary) !important',
     },
     '.bg-tertiary-container': {
-      backgroundColor: 'var(--sys-tertiary-container) !important',
+      backgroundColor: 'var(--mat-sys-tertiary-container) !important',
     },
     '.text-on-tertiary-container': {
-      color: 'var(--sys-on-tertiary-container) !important',
+      color: 'var(--mat-sys-on-tertiary-container) !important',
     },
     '.bg-tertiary-fixed': {
-      backgroundColor: 'var(--sys-tertiary-fixed) !important',
+      backgroundColor: 'var(--mat-sys-tertiary-fixed) !important',
     },
     '.bg-tertiary-fixed-dim': {
-      backgroundColor: 'var(--sys-tertiary-fixed-dim) !important',
+      backgroundColor: 'var(--mat-sys-tertiary-fixed-dim) !important',
     },
     '.text-on-tertiary-fixed': {
-      color: 'var(--sys-on-tertiary-fixed) !important',
+      color: 'var(--mat-sys-on-tertiary-fixed) !important',
     },
     '.text-on-tertiary-fixed-variant': {
-      color: 'var(--sys-on-tertiary-fixed-variant) !important',
+      color: 'var(--mat-sys-on-tertiary-fixed-variant) !important',
     },
 
     '.bg-error': {
-      backgroundColor: 'var(--sys-error) !important',
+      backgroundColor: 'var(--mat-sys-error) !important',
     },
     '.text-error': {
-      color: 'var(--sys-error) !important',
+      color: 'var(--mat-sys-error) !important',
     },
     '.text-on-error': {
-      color: 'var(--sys-on-error) !important',
+      color: 'var(--mat-sys-on-error) !important',
     },
     '.bg-error-container': {
-      backgroundColor: 'var(--sys-error-container) !important',
+      backgroundColor: 'var(--mat-sys-error-container) !important',
     },
     '.text-on-error-container': {
-      color: 'var(--sys-on-error-container) !important',
+      color: 'var(--mat-sys-on-error-container) !important',
     },
   });
 
   addUtilities({
     '.text-display-large': {
-      font: 'var(--sys-display-large) !important',
+      font: 'var(--mat-sys-display-large) !important',
     },
     '.text-display-medium': {
-      font: 'var(--sys-display-medium) !important',
+      font: 'var(--mat-sys-display-medium) !important',
     },
     '.text-display-small': {
-      font: 'var(--sys-display-small) !important',
+      font: 'var(--mat-sys-display-small) !important',
     },
     '.text-headline-large': {
-      font: 'var(--sys-headline-large) !important',
+      font: 'var(--mat-sys-headline-large) !important',
     },
     '.text-headline-medium': {
-      font: 'var(--sys-headline-medium) !important',
+      font: 'var(--mat-sys-headline-medium) !important',
     },
     '.text-headline-small': {
-      font: 'var(--sys-headline-small) !important',
+      font: 'var(--mat-sys-headline-small) !important',
     },
     '.text-title-large': {
-      font: 'var(--sys-title-large) !important',
+      font: 'var(--mat-sys-title-large) !important',
     },
     '.text-title-medium': {
-      font: 'var(--sys-title-medium) !important',
+      font: 'var(--mat-sys-title-medium) !important',
     },
     '.text-title-small': {
-      font: 'var(--sys-title-small) !important',
+      font: 'var(--mat-sys-title-small) !important',
     },
     '.text-body-large': {
-      font: 'var(--sys-body-large) !important',
+      font: 'var(--mat-sys-body-large) !important',
     },
     '.text-body-medium': {
-      font: 'var(--sys-body-medium) !important',
+      font: 'var(--mat-sys-body-medium) !important',
     },
     '.text-body-small': {
-      font: 'var(--sys-body-small) !important',
+      font: 'var(--mat-sys-body-small) !important',
     },
     '.text-label-large': {
-      font: 'var(--sys-label-large) !important',
+      font: 'var(--mat-sys-label-large) !important',
     },
     '.text-label-medium': {
-      font: 'var(--sys-label-medium) !important',
+      font: 'var(--mat-sys-label-medium) !important',
     },
     '.text-label-small': {
-      font: 'var(--sys-label-small) !important',
+      font: 'var(--mat-sys-label-small) !important',
     },
   });
 
@@ -212,23 +212,23 @@ module.exports = plugin(({ addComponents, addUtilities, theme }) => {
     '.msk-mat-card-elevated': {
       position: 'relative',
       boxShadow: 'var(--mat-app-elevation-shadow-level-1)',
-      borderRadius: 'var(--sys-corner-medium)',
-      backgroundColor: 'var(--sys-surface-container-low)',
+      borderRadius: 'var(--mat-sys-corner-medium)',
+      backgroundColor: 'var(--mat-sys-surface-container-low)',
     },
 
     '.msk-mat-card-filled': {
       position: 'relative',
       boxShadow: 'var(--mat-app-elevation-shadow-level-0)',
-      borderRadius: 'var(--sys-corner-medium)',
-      backgroundColor: 'var(--sys-surface-container-highest)',
+      borderRadius: 'var(--mat-sys-corner-medium)',
+      backgroundColor: 'var(--mat-sys-surface-container-highest)',
     },
 
     '.msk-mat-card-outlined': {
       position: 'relative',
       boxShadow: 'var(--mat-app-elevation-shadow-level-0)',
-      borderRadius: 'var(--sys-corner-medium)',
-      backgroundColor: 'var(--sys-surface)',
-      border: '1px solid var(--sys-outline-variant)',
+      borderRadius: 'var(--mat-sys-corner-medium)',
+      backgroundColor: 'var(--mat-sys-surface)',
+      border: '1px solid var(--mat-sys-outline-variant)',
     },
   });
 
@@ -244,7 +244,7 @@ module.exports = plugin(({ addComponents, addUtilities, theme }) => {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
-      color: 'var(--sys-on-surface-variant)',
+      color: 'var(--mat-sys-on-surface-variant)',
     },
     '.msk-list-item-content': {
       display: 'flex',
@@ -255,17 +255,17 @@ module.exports = plugin(({ addComponents, addUtilities, theme }) => {
     '.msk-list-item-content span:nth-child(1)': {
       fontWeight: '500',
       lineHeight: '1.5rem',
-      color: 'var(--sys-on-surface)',
+      color: 'var(--mat-sys-on-surface)',
     },
     '.msk-list-item-content span:nth-child(2)': {
       fontSize: '0.75rem',
       lineHeight: '1rem',
-      color: 'var(--sys-on-surface-variant)',
+      color: 'var(--mat-sys-on-surface-variant)',
     },
     '.msk-list-item-content span:nth-child(3)': {
       fontSize: '0.75rem',
       lineHeight: '1rem',
-      color: 'var(--sys-on-surface-variant)',
+      color: 'var(--mat-sys-on-surface-variant)',
     },
   });
 });
