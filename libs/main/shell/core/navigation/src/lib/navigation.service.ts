6015658259398b9fefbc99be7d5439b77289203f
mohsen77sk <mohsen77sk@gmail.com>
2025-04-30 14:28:24 +0330

    update main-app navigation

--- libs/main/shell/core/navigation/src/lib/navigation.service.ts
+++ libs/main/shell/core/navigation/src/lib/navigation.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TranslocoService } from '@jsverse/transloco';
 import { Observable, ReplaySubject, tap } from 'rxjs';
 import { Navigation } from './navigation.types';
 
 @Injectable({ providedIn: 'root' })
 export class MainNavigationService {
   private _httpClient = inject(HttpClient);
+  private _translocoService = inject(TranslocoService);
   private _navigation: ReplaySubject<Navigation> = new ReplaySubject<Navigation>(1);
 
   // -----------------------------------------------------------------------------------------------------
@@ -27,7 +29,8 @@ export class MainNavigationService {
    * Get all navigation data
    */
   get(): Observable<Navigation> {
-    return this._httpClient.get<Navigation>('assets/api/navigation.json').pipe(
+    const lang_id = this._translocoService.getActiveLang();
+    return this._httpClient.get<Navigation>(`assets/api/${lang_id}/navigation.json`).pipe(
       tap((navigation) => {
         this._navigation.next(navigation);
       })
