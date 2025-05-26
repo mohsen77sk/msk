f73635fe75a144115df023c7c367c12d7f32b092
mohsen77sk <mohsen77sk@gmail.com>
2025-05-26 09:33:43 +0330

    update auth interceptor

--- libs/mirza/shell/core/auth/src/lib/auth.interceptor.ts
+++ libs/mirza/shell/core/auth/src/lib/auth.interceptor.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest } from '@angul
 import { Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
+import { StoreService } from '@msk/mirza/shell/core/store';
 import { AuthService } from './auth.service';
 import { AuthUtils } from './auth.utils';
 
@@ -14,23 +15,23 @@ import { AuthUtils } from './auth.utils';
 export const authInterceptor = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
   const router = inject(Router);
   const authService = inject(AuthService);
+  const storeService = inject(StoreService);
 
-  // Clone the request object
-  let newReq = req.clone();
+  // Clone the request headers object to add new headers
+  let headers = req.headers;
 
-  // Request
-  //
   // If the access token didn't expire, add the Authorization header.
-  // We won't add the Authorization header if the access token expired.
-  // This will force the server to return a "401 Unauthorized" response
-  // for the protected API routes which our response interceptor will
-  // catch and delete the access token from the local storage while logging
-  // the user out from the app.
   if (authService.accessToken && !AuthUtils.isTokenExpired(authService.accessToken)) {
-    newReq = req.clone({
-      headers: req.headers.set('Authorization', 'Bearer ' + authService.accessToken),
-    });
+    headers = headers.append('Authorization', 'Bearer ' + authService.accessToken);
   }
+
+  // If the store is set, add the Store-Id header.
+  if (storeService.currentStore) {
+    headers = headers.append('Store-Id', storeService.currentStore.id.toString());
+  }
+
+  // Clone the request object by new headers
+  const newReq = req.clone({ headers });
 
   // Response
   return next(newReq).pipe(
