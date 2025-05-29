d1a6492776203cb81a731cd1532d1f2075ad9930
mohsen77sk <mohsen77sk@gmail.com>
2025-05-29 16:10:09 +0330

    update mirza vendors

--- libs/mirza/panel/vendors/src/lib/vendors.routes.ts
+++ libs/mirza/panel/vendors/src/lib/vendors.routes.ts
@@ -1,11 +1,38 @@
 import { inject } from '@angular/core';
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, Routes } from '@angular/router';
 import { VendorsService } from './vendors.service';
 import { VendorsComponent } from './vendors.component';
 import { VendorsListComponent } from './list/list.component';
 
 import { scopeLoader } from '@msk/shared/utils/transloco';
 import { provideTranslocoScope } from '@jsverse/transloco';
+import { MskErrorResponse } from '@msk/shared/data-access';
+import { catchError, throwError } from 'rxjs';
+
+/**
+ * Vendor resolver
+ *
+ * @param route
+ * @param state
+ */
+const vendorResolver = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const vendorsService = inject(VendorsService);
+  const router = inject(Router);
+
+  return vendorsService.getVendor(route.paramMap.get('id') ?? 0).pipe(
+    // Error here means the requested contact is not available
+    catchError((error: MskErrorResponse) => {
+      // Log the error
+      console.error(error);
+      // Get the parent url
+      const parentUrl = state.url.split('/').slice(0, -1).join('/');
+      // Navigate to there
+      router.navigateByUrl(parentUrl);
+      // Throw an error
+      return throwError(() => new Error(error.message));
+    })
+  );
+};
 
 export const routes: Routes = [
   {
