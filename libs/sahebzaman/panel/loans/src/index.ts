943a0cbf204245a066fdab2daabab798fd4eaf7e
mohsen77sk <mohsen77sk@gmail.com>
2025-05-31 15:17:57 +0330

    update sahebzaman loan

--- libs/sahebzaman/panel/loans/src/index.ts
+++ libs/sahebzaman/panel/loans/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/loans.component';
 export * from './lib/loans.routes';
+export * from './lib/loans.types';
---
+++ libs/sahebzaman/panel/loans/src/lib/loans.types.ts
@@ -0,0 +1,57 @@
+import { SortDirection } from '@angular/material/sort';
+
+export const DefaultLoanSortId = 'code';
+export const DefaultLoanSortDirection: SortDirection = 'asc';
+
+export interface ICreateLoan {
+  accountId: number;
+  loanTypeId: number;
+  createDate: Date;
+  amount: number;
+  installmentCount: number;
+  installmentInterval: number;
+  interestRates: number;
+  note?: string;
+}
+
+export interface IUpdateAccount {
+  id: number;
+  accountId: number;
+  note?: string;
+}
+
+export class Loan {
+  id: number;
+  code: string;
+  accountId: number;
+  accountCode: string;
+  loanTypeId: number;
+  loanTypeName: string;
+  createDate: Date;
+  closeDate?: Date;
+  amount: number;
+  installmentAmount: number;
+  installmentCount: number;
+  installmentInterval: number;
+  interestRates: number;
+  note?: string;
+  isActive: boolean;
+
+  constructor(input: Loan) {
+    this.id = input.id;
+    this.code = input.code;
+    this.accountId = input.accountId;
+    this.accountCode = input.accountCode;
+    this.loanTypeId = input.loanTypeId;
+    this.loanTypeName = input.loanTypeName;
+    this.createDate = new Date(input.createDate);
+    this.closeDate = input.closeDate ? new Date(input.closeDate) : undefined;
+    this.amount = input.amount;
+    this.installmentAmount = input.installmentAmount;
+    this.installmentCount = input.installmentCount;
+    this.installmentInterval = input.installmentInterval;
+    this.interestRates = input.interestRates;
+    this.note = input.note;
+    this.isActive = input.isActive;
+  }
+}
