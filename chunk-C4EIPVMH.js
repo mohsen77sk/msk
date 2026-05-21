6ca931478feb8945448ea841cc40411fc4efce86
mohsen77sk <mohsen77sk@users.noreply.github.com>
2026-05-21 08:01:22 +0000

    deploy: c7ad85bf7d260ca20d0c5b8a6718d21e2f485b53

---
+++ chunk-C4EIPVMH.js
@@ -0,0 +1,142 @@
+import{I as w,J as M,b as k,n as T,r as I,s as U}from"./chunk-VA4V6LA2.js";import{a as P,b as z,c as B,d as L}from"./chunk-FSACWZFZ.js";import"./chunk-NITXLYHI.js";import{a as F}from"./chunk-PP4OLWFV.js";import{O as D,S as O,T as h}from"./chunk-75HGUJFB.js";import"./chunk-AJZUO2RT.js";import"./chunk-NFZYCFYO.js";import{Ab as r,Eb as l,Fb as t,Gb as e,Hb as y,Rb as x,Tb as _,Ua as a,dc as c,gc as i,hb as A,ic as g,jc as S,kc as C,lc as E,nb as p,xb as v,yb as f,zb as d}from"./chunk-IXY3QUDV.js";import"./chunk-2R6CW7ES.js";function W(n,m){n&1&&(t(0,"div",10),f("msk-collapsed"),v("msk-expanded"),t(1,"span"),i(2,"Animated box"),e()())}function Z(n,m){n&1&&(t(0,"textarea",22),i(1,`              <!-- Using state -->
+              <div class="animated-box" [class.msk-expanded]="isOpen" [class.msk-collapsed]="!isOpen">
+                <span> Animated box </span>
+              </div>
+              <!-- Using *ngIf -->
+              @if (visible) {
+                <div class="animated-box" animate.enter="msk-expanded" animate.leave="msk-collapsed">
+                  <span> Animated box </span>
+                </div>
+              }
+            `),e()),n&2&&l("lang","html")}function N(n,m){n&1&&(t(0,"div",10),v("msk-shake"),t(1,"span"),i(2,"Animated box"),e()())}function j(n,m){n&1&&(t(0,"textarea",22),i(1,`              <!-- Using state -->
+              <div class="animated-box" [class.msk-shake]="isShake">
+                <span> Animated box </span>
+              </div>
+              <!-- Using *ngIf -->
+              @if (visible) {
+                <div class="animated-box" animate.enter="msk-shake">
+                  <span> Animated box </span>
+                </div>
+              }
+            `),e()),n&2&&l("lang","html")}function H(n,m){n&1&&(t(0,"div",10),v("msk-fade-in"),t(1,"span"),i(2,"Animated box"),e()())}function R(n,m){n&1&&(t(0,"textarea",22),i(1,`              <!-- Using state -->
+              <div class="animated-box" [class.msk-fade-in]="isFadeIn">
+                <span> Animated box </div>
+              </div>
+              <!-- Using *ngIf -->
+              @if (visible) {
+              <div class="animated-box" animate.enter="msk-fade-in">
+                <span> Animated box </div>
+              </div>
+              }
+            `),e()),n&2&&l("lang","html")}function G(n,m){n&1&&(t(0,"div",10),f("msk-fade-out"),t(1,"span"),i(2,"Animated box"),e()())}function q(n,m){n&1&&(t(0,"textarea",22),i(1,`              <!-- Using state -->
+              <div class="animated-box" [class.msk-fade-out]="isFadeOut">
+                <span> Animated box </div>
+              </div>
+              <!-- Using *ngIf -->
+              @if (visible) {
+              <div class="animated-box" animate.leave="msk-fade-out">
+                <span> Animated box </div>
+              </div>
+              }
+            `),e()),n&2&&l("lang","html")}function J(n,m){n&1&&(t(0,"div",10),v("msk-zoom-in"),t(1,"span"),i(2,"Animated box"),e()())}function K(n,m){n&1&&(t(0,"textarea",22),i(1,`              <!-- Using state -->
+              <div class="animated-box" [class.msk-zoom-in]="isZoomIn">
+                <span> Animated box </div>
+              </div>
+              <!-- Using *ngIf -->
+              @if (visible) {
+              <div class="animated-box" animate.enter="msk-zoom-in">
+                <span> Animated box </div>
+              </div>
+              }
+            `),e()),n&2&&l("lang","html")}function Q(n,m){n&1&&(t(0,"div",10),f("msk-zoom-out"),t(1,"span"),i(2,"Animated box"),e()())}function X(n,m){n&1&&(t(0,"textarea",22),i(1,`              <!-- Using state -->
+              <div class="animated-box" [class.msk-zoom-out]="isZoomOut">
+                <span> Animated box </div>
+              </div>
+              <!-- Using *ngIf -->
+              @if (visible) {
+              <div class="animated-box" animate.leave="msk-zoom-out">
+                <span> Animated box </div>
+              </div>
+              }
+            `),e()),n&2&&l("lang","html")}function Y(n,m){n&1&&(t(0,"div",10),v("msk-slide-in-top"),t(1,"span"),i(2,"Animated box"),e()())}function $(n,m){n&1&&(t(0,"textarea",22),i(1,`                  <!-- Using state -->
+                  <div class="animated-box" [class.msk-slide-in-top]="isSlideIn">
+                    <span> Animated box </div>
+                  </div>
+                  <!-- Using *ngIf -->
+                  @if (visible) {
+                    <div class="animated-box" animate.enter="msk-slide-in-top">
+                      <span> Animated box </div>
+                    </div>
+                  }
+                `),e()),n&2&&l("lang","html")}function tt(n,m){if(n&1&&(t(0,"mat-tab-group",8)(1,"mat-tab",9)(2,"div",10)(3,"span"),i(4,"Animated box"),e()()(),t(5,"mat-tab",11),d(6,Y,3,0,"div",10),e(),t(7,"mat-tab",12),p(8,$,2,1,"ng-template",13),e()()),n&2){let s=_();l("animationDuration","0ms"),a(2),c("msk-slide-in-top",s.slideInOn),a(4),r(s.slideInOn?6:-1)}}function et(n,m){n&1&&(t(0,"div",10),v("msk-slide-in-bottom"),t(1,"span"),i(2,"Animated box"),e()())}function nt(n,m){n&1&&(t(0,"textarea",22),i(1,`                  <!-- Using state -->
+                  <div class="animated-box" [class.msk-slide-in-bottom]="isSlideIn">
+                    <span> Animated box </div>
+                  </div>
+                  <!-- Using *ngIf -->
+                  @if (visible) {
+                    <div class="animated-box" animate.enter="msk-slide-in-bottom">
+                      <span> Animated box </div>
+                    </div>
+                  }
+                `),e()),n&2&&l("lang","html")}function it(n,m){if(n&1&&(t(0,"mat-tab-group",8)(1,"mat-tab",9)(2,"div",10)(3,"span"),i(4,"Animated box"),e()()(),t(5,"mat-tab",11),d(6,et,3,0,"div",10),e(),t(7,"mat-tab",12),p(8,nt,2,1,"ng-template",13),e()()),n&2){let s=_();l("animationDuration","0ms"),a(2),c("msk-slide-in-bottom",s.slideInOn),a(4),r(s.slideInOn?6:-1)}}function at(n,m){n&1&&(t(0,"div",10),v("msk-slide-in-left"),t(1,"span"),i(2,"Animated box"),e()())}function mt(n,m){n&1&&(t(0,"textarea",22),i(1,`                  <!-- Using state -->
+                  <div class="animated-box" [class.msk-slide-in-left]="isSlideIn">
+                    <span> Animated box </div>
+                  </div>
+                  <!-- Using *ngIf -->
+                  @if (visible) {
+                    <div class="animated-box" animate.enter="msk-slide-in-left">
+                      <span> Animated box </div>
+                    </div>
+                  }
+                `),e()),n&2&&l("lang","html")}function ot(n,m){if(n&1&&(t(0,"mat-tab-group",8)(1,"mat-tab",9)(2,"div",10)(3,"span"),i(4,"Animated box"),e()()(),t(5,"mat-tab",11),d(6,at,3,0,"div",10),e(),t(7,"mat-tab",12),p(8,mt,2,1,"ng-template",13),e()()),n&2){let s=_();l("animationDuration","0ms"),a(2),c("msk-slide-in-left",s.slideInOn),a(4),r(s.slideInOn?6:-1)}}function lt(n,m){n&1&&(t(0,"div",10),v("msk-slide-in-right"),t(1,"span"),i(2,"Animated box"),e()())}function st(n,m){n&1&&(t(0,"textarea",22),i(1,`                  <!-- Using state -->
+                  <div class="animated-box" [class.msk-slide-in-right]="isSlideIn">
+                    <span> Animated box </div>
+                  </div>
+                  <!-- Using *ngIf -->
+                  @if (visible) {
+                    <div class="animated-box" animate.enter="msk-slide-in-right">
+                      <span> Animated box </div>
+                    </div>
+                  }
+                `),e()),n&2&&l("lang","html")}function dt(n,m){if(n&1&&(t(0,"mat-tab-group",8)(1,"mat-tab",9)(2,"div",10)(3,"span"),i(4,"Animated box"),e()()(),t(5,"mat-tab",11),d(6,lt,3,0,"div",10),e(),t(7,"mat-tab",12),p(8,st,2,1,"ng-template",13),e()()),n&2){let s=_();l("animationDuration","0ms"),a(2),c("msk-slide-in-right",s.slideInOn),a(4),r(s.slideInOn?6:-1)}}function rt(n,m){n&1&&(t(0,"div",10),f("msk-slide-out-top"),t(1,"span"),i(2,"Animated box"),e()())}function pt(n,m){n&1&&(t(0,"textarea",22),i(1,`                  <!-- Using state -->
+                  <div class="animated-box" [class.msk-slide-out-top]="isSlideIn">
+                    <span> Animated box </div>
+                  </div>
+                  <!-- Using *ngIf -->
+                  @if (visible) {
+                    <div class="animated-box" animate.leave="msk-slide-out-top">
+                      <span> Animated box </div>
+                    </div>
+                  }
+                `),e()),n&2&&l("lang","html")}function ct(n,m){if(n&1&&(t(0,"mat-tab-group",8)(1,"mat-tab",9)(2,"div",10)(3,"span"),i(4,"Animated box"),e()()(),t(5,"mat-tab",11),d(6,rt,3,0,"div",10),e(),t(7,"mat-tab",12),p(8,pt,2,1,"ng-template",13),e()()),n&2){let s=_();l("animationDuration","0ms"),a(2),c("msk-slide-out-top",s.slideOutOn),a(4),r(s.slideOutOn?6:-1)}}function bt(n,m){n&1&&(t(0,"div",10),f("msk-slide-out-bottom"),t(1,"span"),i(2,"Animated box"),e()())}function ut(n,m){n&1&&(t(0,"textarea",22),i(1,`                  <!-- Using state -->
+                  <div class="animated-box" [class.msk-slide-out-bottom]="isSlideIn">
+                    <span> Animated box </div>
+                  </div>
+                  <!-- Using *ngIf -->
+                  @if (visible) {
+                    <div class="animated-box" animate.leave="msk-slide-out-bottom">
+                      <span> Animated box </div>
+                    </div>
+                  }
+                `),e()),n&2&&l("lang","html")}function vt(n,m){if(n&1&&(t(0,"mat-tab-group",8)(1,"mat-tab",9)(2,"div",10)(3,"span"),i(4,"Animated box"),e()()(),t(5,"mat-tab",11),d(6,bt,3,0,"div",10),e(),t(7,"mat-tab",12),p(8,ut,2,1,"ng-template",13),e()()),n&2){let s=_();l("animationDuration","0ms"),a(2),c("msk-slide-out-bottom",s.slideOutOn),a(4),r(s.slideOutOn?6:-1)}}function xt(n,m){n&1&&(t(0,"div",10),f("msk-slide-out-left"),t(1,"span"),i(2,"Animated box"),e()())}function _t(n,m){n&1&&(t(0,"textarea",22),i(1,`                  <!-- Using state -->
+                  <div class="animated-box" [class.msk-slide-out-left]="isSlideIn">
+                    <span> Animated box </div>
+                  </div>
+                  <!-- Using *ngIf -->
+                  @if (visible) {
+                    <div class="animated-box" animate.leave="msk-slide-out-left">
+                      <span> Animated box </div>
+                    </div>
+                  }
+                `),e()),n&2&&l("lang","html")}function ft(n,m){if(n&1&&(t(0,"mat-tab-group",8)(1,"mat-tab",9)(2,"div",10)(3,"span"),i(4,"Animated box"),e()()(),t(5,"mat-tab",11),d(6,xt,3,0,"div",10),e(),t(7,"mat-tab",12),p(8,_t,2,1,"ng-template",13),e()()),n&2){let s=_();l("animationDuration","0ms"),a(2),c("msk-slide-out-left",s.slideOutOn),a(4),r(s.slideOutOn?6:-1)}}function gt(n,m){n&1&&(t(0,"div",10),f("msk-slide-out-right"),t(1,"span"),i(2,"Animated box"),e()())}function St(n,m){n&1&&(t(0,"textarea",22),i(1,`                  <!-- Using state -->
+                  <div class="animated-box" [class.msk-slide-out-right]="isSlideIn">
+                    <span> Animated box </div>
+                  </div>
+                  <!-- Using *ngIf -->
+                  @if (visible) {
+                    <div class="animated-box" animate.leave="msk-slide-out-right">
+                      <span> Animated box </div>
+                    </div>
+                  }
+                `),e()),n&2&&l("lang","html")}function Ct(n,m){if(n&1&&(t(0,"mat-tab-group",8)(1,"mat-tab",9)(2,"div",10)(3,"span"),i(4,"Animated box"),e()()(),t(5,"mat-tab",11),d(6,gt,3,0,"div",10),e(),t(7,"mat-tab",12),p(8,St,2,1,"ng-template",13),e()()),n&2){let s=_();l("animationDuration","0ms"),a(2),c("msk-slide-out-right",s.slideOutOn),a(4),r(s.slideOutOn?6:-1)}}var V=class n{expandVisible=!0;shakeOn=!0;fadeInOn=!0;fadeOutOn=!0;zoomInOn=!0;zoomOutOn=!0;slideInOn=!0;slideOutOn=!0;slideInDir="top";slideOutDir="top";toggleBool(m){this[m]=!1,setTimeout(()=>{this[m]=!0},500)}static \u0275fac=function(s){return new(s||n)};static \u0275cmp=A({type:n,selectors:[["doc-animations"]],decls:147,vars:32,consts:[["dir","ltr",1,"page-layout","content-scroll","md:py-4"],[1,"content","prose","max-w-full"],[1,"sticky-header"],[1,"example-viewer","mt-8"],[1,"header"],[1,"title"],[1,"action"],["matButton","",3,"click"],["mat-stretch-tabs","false",3,"animationDuration"],["label","State Preview"],[1,"animated-box"],["label","NgIf Preview"],["label","HTML"],["matTabContent",""],[1,"mt-8","w-full"],[3,"valueChange","value"],["value","top"],["value","bottom"],["value","left"],["value","right"],[1,"example-viewer","mt-1"],[1,"mb-6"],["msk-highlight","",3,"lang"]],template:function(s,o){if(s&1&&(t(0,"div",0)(1,"div",1)(2,"h3",2),i(3,"Animations"),e(),t(4,"div",3)(5,"div",4)(6,"div",5),i(7,"Expand / Collapse"),e(),t(8,"div",6)(9,"button",7),x("click",function(){return o.expandVisible=!o.expandVisible}),i(10,"Toggle state"),e()()(),t(11,"mat-tab-group",8)(12,"mat-tab",9)(13,"div",10)(14,"span"),i(15,"Animated box"),e()()(),t(16,"mat-tab",11),d(17,W,3,0,"div",10),e(),t(18,"mat-tab",12),p(19,Z,2,1,"ng-template",13),e()()(),t(20,"div",3)(21,"div",4)(22,"div",5),i(23,"Shake"),e(),t(24,"div",6)(25,"button",7),x("click",function(){return o.toggleBool("shakeOn")}),i(26,"Toggle state"),e()()(),t(27,"mat-tab-group",8)(28,"mat-tab",9)(29,"div",10)(30,"span"),i(31,"Animated box"),e()()(),t(32,"mat-tab",11),d(33,N,3,0,"div",10),e(),t(34,"mat-tab",12),p(35,j,2,1,"ng-template",13),e()()(),t(36,"div",3)(37,"div",4)(38,"div",5),i(39,"Fade In"),e(),t(40,"div",6)(41,"button",7),x("click",function(){return o.toggleBool("fadeInOn")}),i(42,"Toggle state"),e()()(),t(43,"mat-tab-group",8)(44,"mat-tab",9)(45,"div",10)(46,"span"),i(47,"Animated box"),e()()(),t(48,"mat-tab",11),d(49,H,3,0,"div",10),e(),t(50,"mat-tab",12),p(51,R,2,1,"ng-template",13),e()()(),t(52,"div",3)(53,"div",4)(54,"div",5),i(55,"Fade Out"),e(),t(56,"div",6)(57,"button",7),x("click",function(){return o.toggleBool("fadeOutOn")}),i(58,"Toggle state"),e()()(),t(59,"mat-tab-group",8)(60,"mat-tab",9)(61,"div",10)(62,"span"),i(63,"Animated box"),e()()(),t(64,"mat-tab",11),d(65,G,3,0,"div",10),e(),t(66,"mat-tab",12),p(67,q,2,1,"ng-template",13),e()()(),t(68,"div",3)(69,"div",4)(70,"div",5),i(71,"Zoom In"),e(),t(72,"div",6)(73,"button",7),x("click",function(){return o.toggleBool("zoomInOn")}),i(74,"Toggle state"),e()()(),t(75,"mat-tab-group",8)(76,"mat-tab",9)(77,"div",10)(78,"span"),i(79,"Animated box"),e()()(),t(80,"mat-tab",11),d(81,J,3,0,"div",10),e(),t(82,"mat-tab",12),p(83,K,2,1,"ng-template",13),e()()(),t(84,"div",3)(85,"div",4)(86,"div",5),i(87,"Zoom Out"),e(),t(88,"div",6)(89,"button",7),x("click",function(){return o.toggleBool("zoomOutOn")}),i(90,"Toggle state"),e()()(),t(91,"mat-tab-group",8)(92,"mat-tab",9)(93,"div",10)(94,"span"),i(95,"Animated box"),e()()(),t(96,"mat-tab",11),d(97,Q,3,0,"div",10),e(),t(98,"mat-tab",12),p(99,X,2,1,"ng-template",13),e()()(),t(100,"mat-form-field",14)(101,"mat-label"),i(102,"Slide In Direction"),e(),t(103,"mat-select",15),E("valueChange",function(u){return C(o.slideInDir,u)||(o.slideInDir=u),u}),t(104,"mat-option",16),i(105,"Top"),e(),t(106,"mat-option",17),i(107,"Bottom"),e(),t(108,"mat-option",18),i(109,"Left"),e(),t(110,"mat-option",19),i(111,"Right"),e()()(),t(112,"div",20)(113,"div",4)(114,"div",5),i(115),e(),t(116,"div",6)(117,"button",7),x("click",function(){return o.toggleBool("slideInOn")}),i(118,"Toggle state"),e()()(),d(119,tt,9,4,"mat-tab-group",8)(120,it,9,4,"mat-tab-group",8)(121,ot,9,4,"mat-tab-group",8)(122,dt,9,4,"mat-tab-group",8),e(),t(123,"mat-form-field",14)(124,"mat-label"),i(125,"Slide Out Direction"),e(),t(126,"mat-select",15),E("valueChange",function(u){return C(o.slideOutDir,u)||(o.slideOutDir=u),u}),t(127,"mat-option",16),i(128,"Top"),e(),t(129,"mat-option",17),i(130,"Bottom"),e(),t(131,"mat-option",18),i(132,"Left"),e(),t(133,"mat-option",19),i(134,"Right"),e()()(),t(135,"div",20)(136,"div",4)(137,"div",5),i(138),e(),t(139,"div",6)(140,"button",7),x("click",function(){return o.toggleBool("slideOutOn")}),i(141,"Toggle state"),e()()(),d(142,ct,9,4,"mat-tab-group",8)(143,vt,9,4,"mat-tab-group",8)(144,ft,9,4,"mat-tab-group",8)(145,Ct,9,4,"mat-tab-group",8),e(),y(146,"div",21),e()()),s&2){let b,u;a(11),l("animationDuration","0ms"),a(2),c("msk-expanded",o.expandVisible)("msk-collapsed",!o.expandVisible),a(4),r(o.expandVisible?17:-1),a(10),l("animationDuration","0ms"),a(2),c("msk-shake",o.shakeOn),a(4),r(o.shakeOn?33:-1),a(10),l("animationDuration","0ms"),a(2),c("msk-fade-in",o.fadeInOn),a(4),r(o.fadeInOn?49:-1),a(10),l("animationDuration","0ms"),a(2),c("msk-fade-out",o.fadeOutOn),a(4),r(o.fadeOutOn?65:-1),a(10),l("animationDuration","0ms"),a(2),c("msk-zoom-in",o.zoomInOn),a(4),r(o.zoomInOn?81:-1),a(10),l("animationDuration","0ms"),a(2),c("msk-zoom-out",o.zoomOutOn),a(4),r(o.zoomOutOn?97:-1),a(6),S("value",o.slideInDir),a(12),g("Slide In (",o.slideInDir,")"),a(4),r((b=o.slideInDir)==="top"?119:b==="bottom"?120:b==="left"?121:b==="right"?122:-1),a(7),S("value",o.slideOutDir),a(12),g("Slide Out (",o.slideOutDir,")"),a(4),r((u=o.slideOutDir)==="top"?142:u==="bottom"?143:u==="left"?144:u==="right"?145:-1)}},dependencies:[F,L,D,P,z,B,h,O,M,I,T,w,k,U],styles:[`doc-animations .mat-mdc-tab-body-wrapper .mat-mdc-tab-body .mat-mdc-tab-body-content{min-height:152px;display:flex;justify-content:center;overflow:hidden}doc-animations .mat-mdc-tab-body-wrapper .mat-mdc-tab-body .mat-mdc-tab-body-content .msk-highlight{width:100%}.animated-box{width:120px;height:120px;overflow:hidden;border-radius:4px;cursor:pointer;-webkit-user-select:none;user-select:none;box-shadow:var(--mat-sys-level3);background-color:var(--mat-sys-tertiary-container)}.animated-box>span{display:flex;align-items:center;justify-content:center;flex:1 0 auto;width:100%;height:100%;text-align:center;font-size:12px;font-weight:500;color:var(--mat-sys-on-tertiary-container)}
+`],encapsulation:2})};export{V as DocsAnimationsComponent};
