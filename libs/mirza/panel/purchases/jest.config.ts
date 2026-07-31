611002416924d6aead932ad25917866f957936a5
mohsen77sk <mohsen77sk@gmail.com>
2026-07-31 11:42:56 +0330

    update

--- libs/mirza/panel/purchases/jest.config.ts
+++ libs/mirza/panel/purchases/jest.config.ts
@@ -12,7 +12,7 @@ export default {
       },
     ],
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@jsverse)'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
