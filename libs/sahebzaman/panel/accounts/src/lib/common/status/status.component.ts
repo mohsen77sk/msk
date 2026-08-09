b4d91c22cf1ce2eec9740bd1d862b6d7ce2c363b
mohsen77sk <mohsen77sk@gmail.com>
2026-08-09 17:56:55 +0330

    refactor(status): enhance status component styling and logic

--- libs/sahebzaman/panel/accounts/src/lib/common/status/status.component.ts
+++ libs/sahebzaman/panel/accounts/src/lib/common/status/status.component.ts
@@ -1,15 +1,24 @@
-import { booleanAttribute, Component, input, ViewEncapsulation } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { booleanAttribute, Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'sz-accounts-status',
   templateUrl: './status.component.html',
-  styles: 'sz-accounts-status { display: flex }',
   encapsulation: ViewEncapsulation.None,
-  imports: [MatIconModule, MatTooltipModule, TranslocoDirective],
+  imports: [TranslocoDirective],
+  host: {
+    class: 'flex text-label-small rounded-full px-2 py-0.5',
+  },
 })
 export class AccountsStatusComponent {
   value = input(true, { transform: booleanAttribute });
+
+  @HostBinding('class')
+  get hostClass() {
+    if (this.value()) {
+      return 'bg-green-100 text-green-700 dark:bg-green-950 dark:text-green-300';
+    } else {
+      return 'bg-red-100 text-red-700 dark:bg-red-950 dark:text-red-300';
+    }
+  }
 }
